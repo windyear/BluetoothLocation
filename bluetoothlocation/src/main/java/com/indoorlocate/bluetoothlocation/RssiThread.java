401da3c3e98b9974840d2d0ae19556e6f481e49d
windyear <1280027646@qq.com>
2016-04-03 20:42:19 +0800

    new Thread

---
+++ bluetoothlocation/src/main/java/com/indoorlocate/bluetoothlocation/RssiThread.java
@@ -0,0 +1,14 @@
+package com.indoorlocate.bluetoothlocation;
+
+import android.util.Log;
+
+
+/**
+ * Created by Xuwen on 2016/4/3.
+ */
+public class RssiThread extends Thread {
+    @Override
+    public void run() {
+        Log.v("MainActivity","dd");
+    }
+}
